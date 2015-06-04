a4cc73682ff6901b08d5d8129b69fd9200af359d
ChoChun <tigger19870528@hotmail.com>
2015-06-04 13:52:59 +0800

    Ajax傳Json資料到php，php會以陣列解讀 Ajax傳陣列資料到php，php會以陣列解讀

--- ajax/ajax.php
+++ ajax/ajax.php
@@ -1,4 +1,19 @@
 <?php
+//var
+/*
 if($_POST['id']=="change") echo "yes, it has changed!";
 else echo "nothing";
+*/
+
+
+//array
+/*
+foreach($_POST['id'] as $dog)
+       {
+	   echo $dog;
+	   }
+*/
+	   
+//json
+var_dump($_POST['id']);
 ?>
